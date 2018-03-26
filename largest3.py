c40f313b740ed1b941a4dd37a2eebaf9e1cfb53c
MSanthiya <37657240+MSanthiya@users.noreply.github.com>
2018-03-26 16:03:28 +0530

    Create largest3.py

---
+++ largest3.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+if(a>b and a>c):
+  print("a is a greater number")
+elif(b>a and b>c):
+    print("b is a greater number")
+else:
+      print("c is a greater number")
